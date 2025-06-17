5a54216adb885a819379c65b726b792f37e29d8d
Saksham Yadav <infosakshamyadav@gmail.com>
2025-06-17 00:19:16 -0500

    feat:navbar and homepage

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="w-100 p-5 flex justify-between">
+      <div>
+        <h2 className="text-5xl font-extrabold ms-4 p-5">Aina</h2>
+      </div>
+      <div className="text-2xl p-3 me-7 space-x-6 flex ">
+        <h2 className="p-2">About</h2>
+        <h2 className="p-2">Features</h2>
+        <h2 className="p-2">Contact</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Home</h1>
-    </div>
+    <main className="w-full h-100 bg-[#f2eaa4] m-0 p-0">
+      <Navbar />
+      <div className="flex justify-evenly">
+        <div className="m-3 ps-7">
+          <h2 className="text-4xl font-bold">
+            Talk through your <br /> feelings with an <br /> AI mental therapy
+            agent
+          </h2>
+          <p>Aina means mirror to reflect yourself</p>
+        </div>
+        <div className="">
+          <img src="/icon.png" alt="mirror-icon" />
+        </div>
+      </div>
+    </main>
   );
 };
 
