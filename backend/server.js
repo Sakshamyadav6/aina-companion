64aac07492fa2ba7ce76273d88130ea7c7c797c3
Saksham Yadav <infosakshamyadav@gmail.com>
2025-07-25 01:35:50 -0500

    changes

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,10 @@ app.use(passport.session());
 // 3. CORS (if needed)
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      process.env.CLIENT_URL || "http://localhost:5173",
+      "https://ainai.vercel.app",
+    ],
     credentials: true,
   })
 );
